b405295bb5e66f4f1a6af51ef1fd451b7bbc7be2
Gaspare <Gaspare@10.6.2.230>
2017-05-16 09:22:52 +0200

    GUI

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,53 @@
+/**
+ * Sample Skeleton for 'MetroDeParis.fxml' Controller Class
+ */
+
 package it.polito.tdp.metrodeparis;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metrodeparis.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
 public class MetroDeParisController {
 	
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="cmbPartenza"
+    private ComboBox<?> cmbPartenza; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cmbArrivo"
+    private ComboBox<?> cmbArrivo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doCalcolaPercorso(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert cmbPartenza != null : "fx:id=\"cmbPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert cmbArrivo != null : "fx:id=\"cmbArrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		// TODO Auto-generated method stub
+		this.model= model;
+		
+	}
 }
